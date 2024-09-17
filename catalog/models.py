5c1cda4ff6d43683ca60a7fa4b4c42b1275aa562
Карен <karen.karakeyan@yandex.ru>
2024-09-18 01:20:03 +0300

    создание и редактирования файлов views, urls, forms, templates

--- catalog/models.py
+++ catalog/models.py
@@ -1,39 +1,51 @@
 from django.db import models
 from datetime import datetime
 
+# константа для полей с возможными нулевыми значениями
 NULLABLE = {'blank': True, 'null': True}
 
-
+# модель категорий продуктов
 class Category(models.Model):
-    category_name = models.CharField(
-        max_length=100, verbose_name='название категории')
-    description = models.TextField()
-    products_in_category = models.IntegerField(**NULLABLE)
+    category_name = models.CharField(max_length=100, 
+        verbose_name='название категории')
+    description = models.TextField(
+        )
+    products_in_category = models.IntegerField(
+        **NULLABLE)
 
     def __str__(self):
-        return f'{self.pk} содержит {self.category_name}'
+        return f'{self.pk} - {self.category_name}'
 
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
         ordering = ('category_name',)
 
-
+# модель продуктов
 class Product(models.Model):
     product_name = models.CharField(
-        max_length=100, verbose_name='название продукта')
-    product_description = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    product_image = models.ImageField(**NULLABLE)
-    price = models.DecimalField(max_digits=6, decimal_places=2, **NULLABLE)
-    created_at = models.DateField(default=datetime.now)
-    update_at = models.DateField(default=datetime.now)
+        max_length=100, 
+        verbose_name='название продукта')
+    product_description = models.TextField(
+        )
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.CASCADE)
+    product_image = models.ImageField(
+        **NULLABLE)
+    price = models.DecimalField(
+        max_digits=6, 
+        decimal_places=2, 
+        **NULLABLE)
+    created_at = models.DateField(
+        default=datetime.now)
+    update_at = models.DateField(
+        default=datetime.now)
 
-    # возвращает строку сожержащую номер активной версии, если нет активной версии возвращает None
+# возвращает строку сожержащую номер активной версии, если нет активной версии возвращает None
     @property
     def active_version(self):
-        # type: ignore
-        return Release.object.filter(product=self, is_active=True).first()
+        return  Release.objects.filter(product=self, is_active=True).first()
 
     def __str__(self):
         return f'{self.product_name} стоит {self.price} находится в {self.category}'
@@ -43,18 +55,36 @@ class Product(models.Model):
         verbose_name_plural = 'продукты'
         ordering = ('product_name',)
 
- # модель версий продуктов
+# модель версий продуктов
 class Release(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт', help_text= 'Выберте продукт', default='empty')
-    version = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='Версия', help_text='Введите версию продукта')
-    version_name = models.CharField(max_length=100, verbose_name='Название версии', help_text='Введите название версии продукта', **NULLABLE)
-    is_Active = models.BooleanField(default=False, verbose_name='Активная версия')
+    product_id = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        verbose_name='Продукт',
+        help_text='Выберите продукт',
+        default = 'empty',)
+
+    version = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+        default=00.01,
+        verbose_name='Номер версии',
+        help_text='Введите номер версии продукта',)
+
+    version_name = models.CharField(
+        max_length=100, 
+        verbose_name='название версии',
+        help_text='Введите имя версии продукта',
+        **NULLABLE,)
+        
+    is_Active = models.BooleanField(
+        default=False,
+        help_text='Укажите является ли версия активной',)
 
     def __str__(self):
-        return f'{self.version} {"активная" if self.is_Active  else "неактивная"}'
+        return f'{self.version} {"активная" if self.is_Active else "неактивная"}'
 
     class Meta:
         verbose_name = 'версия'
         verbose_name_plural = 'версии'
         ordering = ('version_name',)
-        
